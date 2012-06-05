bdb986e67476c762e1119913d71a0645a296b356
Nick Prater <nick@npbroadcast.com>
2012-06-05 16:55:52 +0100

    Cosmetic code changes to clarify that archive files can be periods other than hour
    Previously archive files were always hour-based. We now permit arbitrary periods, so
    archive files might last (for example) a minute, hour, or day.
    
    This commit alters variable names and comments to reflect this. No functionality
    is changed.

--- src/rotter.c
+++ src/rotter.c
@@ -462,7 +462,7 @@ static int rotter_open_file(rotter_ringbuffer_t *ringbuffer)
 static int rotter_close_file(rotter_ringbuffer_t *ringbuffer)
 {
   rotter_info( "Closing file for ringbuffer %c.", ringbuffer->label);
-  encoder->close(ringbuffer->file_handle, ringbuffer->hour_start);
+  encoder->close(ringbuffer->file_handle, ringbuffer->period_start);
   ringbuffer->close_file = 0;
   ringbuffer->file_handle = NULL;
   return 0;
@@ -537,7 +537,7 @@ static int init_ringbuffers()
     }
 
     ringbuffers[b]->label = label;
-    ringbuffers[b]->hour_start = 0;
+    ringbuffers[b]->period_start = 0;
     ringbuffers[b]->file_handle = NULL;
     ringbuffers[b]->overflow = 0;
     ringbuffers[b]->close_file = 0;
